191a049a9f1bb9ad259ac554c3b652dcb74b6be4
Juan Luis Grandon <jota.lg19@gmail.com>
2025-08-28 15:41:39 -0400

    fix: restore dashboard

--- public/src/app.js
+++ public/src/app.js
@@ -1,6 +1,4 @@
 import Dashboard from './components/pages/dashboard';
-import TestPage from './components/pages/testPage';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import queryString from 'query-string';
@@ -19,7 +17,6 @@ if (queryParams) {
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      {/*<Route path='/' component={Dashboard}/>*/}
-      <Route path='/' component={TestPage}/>
+      <Route path='/' component={Dashboard}/>
     </Switch>
   </BrowserRouter>, document.getElementById('root'));
