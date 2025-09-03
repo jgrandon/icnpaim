ed78cbb2fd8934da2e94622d4d9c3f8e7266bc1f
Juan Luis Grandon <jota.lg19@gmail.com>
2025-09-03 13:39:24 -0400

    fix (bff/units): add course and unit codes to units object

--- server/src/app/handlers/units.js
+++ server/src/app/handlers/units.js
@@ -7,8 +7,34 @@ export async function getUnits( courseId ) {
 
 	return response.data.map( u => ({
 		id: u.id,
+		courseCode: getCourseCode(u.title),
 		status: u.status,
-		title: u.title.rendered,
-		content: u.content.rendered
+		title: getTitle(u.title),
+		content: u.content.rendered,
+		unitCode: getUnitCode(u.title)
 	}))
+}
+
+function getTitle ( title) {
+	const maskedTitle =
+		title.rendered
+			.replaceAll('| ', '')
+			.split(' ')
+			.splice(3)
+			.join(' ')
+	return maskedTitle
+}
+
+function getCourseCode ( title ) {
+	return title.rendered.split(' ').splice(0,2).join(' ')
+}
+
+function getUnitCode ( title ) {
+	const code =
+		title.rendered
+			.replaceAll('| ', '')
+			.split(' ')
+			.splice(0,3)
+			.join(' ')
+	return code
 }
