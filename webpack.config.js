690de8d7a1cb0b0c87501d8cf3dc6a84a02f5fca
Juan Luis Grandon <jota.lg19@gmail.com>
2025-09-16 17:47:15 -0300

    chore : update webpack to load css files

--- webpack.config.js
+++ webpack.config.js
@@ -46,6 +46,19 @@ module.exports = {
             ]
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true, // Enable CSS Modules
+            },
+          },
+        ],
+        exclude: /node_modules/,
       }
     ]
   },
