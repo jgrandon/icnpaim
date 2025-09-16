1930edc2bb06552a163ad6b7aa05838bac462989
Juan Luis Grandon <jota.lg19@gmail.com>
2025-09-15 21:05:40 -0300

    fix (bff/grades): change function export

--- server/src/app/handlers/columns.js
+++ server/src/app/handlers/columns.js
@@ -1,6 +1,6 @@
 import BlackBoardApiClient from '../clients/blackboard'
 
-export default async function getColumnByContent (contentId) {
+export async function getColumnByContent (contentId) {
     const apiClient = BlackBoardApiClient.getClient()
     const request = await apiClient.get(`/v1/courses/${courseId}/contents/${contentId}`)
     const column = request.data
