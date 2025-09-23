0cc6e14e1bd5b5870de50580790d54321c40a465
Juan Luis Grandon <jota.lg19@gmail.com>
2025-09-23 12:44:23 -0300

    styles (cardHeader): update dimensions, colors and display

--- public/src/components/organisms/contentCard/header.js
+++ public/src/components/organisms/contentCard/header.js
@@ -5,7 +5,7 @@ import {
     RadioButtonUnchecked as Unchecked
 } from '@material-ui/icons'
 import { Icon } from './Icon'
-import { _OK_GREEN } from './colors'
+import { _OK_GREEN, _INACTIVE_GRAY } from './colors'
 
 export function CardHeader(props) {
     const { card, color } = props
@@ -13,12 +13,23 @@ export function CardHeader(props) {
 		width: 20,
 		height: 20,
 		borderRadius: 10,
-		border: `2px solid ${_OK_GREEN}`
+		color: card.completed ? _OK_GREEN : _INACTIVE_GRAY
 	}
 
-    return <div>
+    return <div style={{
+                display: grid,
+                gridTemplateColumns: '1fr 1fr'
+            }}
+        >
         <div style={{
-            backgroundColor: color
+            backgroundColor: color,
+            display: 'grid',
+            padding: '5px',
+            gridTemplateColumns: '1fr 2fr',
+            borderRadius: '10px',
+            alignItems: 'center',
+            justifyItems: 'center',
+            justifyContent: 'center'
         }}>
             <Icon 
                 search={card.tipoActividad}
@@ -26,10 +37,10 @@ export function CardHeader(props) {
             />
             <Typography
                 variant="body2"
-                color={'white'}
                 style={{
                     flexGrow: 1,
-                    fontSize: 12
+                    fontSize: 12,
+                    color: white
                 }}
             >
                 {card.tipoActividad || 'actividad'}
