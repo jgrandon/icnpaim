662e038851ba93afbb77839064f8bda2a40e1300
Juan Luis Grandon <jota.lg19@gmail.com>
2025-09-23 14:50:25 -0300

    styles (contentHeader): change display

--- public/src/components/organisms/contentCard/header.js
+++ public/src/components/organisms/contentCard/header.js
@@ -18,8 +18,8 @@ export function CardHeader(props) {
 	}
 
     return <div style={{
-            display: 'grid',
-            gridTemplateColumns: '1fr 1fr'
+            display: 'flex',
+            justifyContent: 'space-between'
         }}
     >
         <div 
