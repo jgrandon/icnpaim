e7b4fa48ecac1b08ae71ac81616615b6ca45ad07
Juan Luis Grandon <jota.lg19@gmail.com>
2025-09-29 20:38:18 -0300

    fix (bff/evaluation): rename variable

--- server/src/app/db/blackboard.js
+++ server/src/app/db/blackboard.js
@@ -50,7 +50,7 @@ export async function getColumnMaxScore(columnId) {
     }
 }
 
-export function updateColumnMaxScore(columnId, maxGrade) {
+export function updateColumnMaxScore(columnId, maxScore) {
     db.push('/column', {
         [columnId] : {maxScore}
     })
