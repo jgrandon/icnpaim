e362faec1ad0ba487759836e753c5125bd2002e2
Juan Luis Grandon <jota.lg19@gmail.com>
2025-09-29 18:42:40 -0300

    fix (bff/evaluation): update variable call

--- server/src/app/clients/blackboard.js
+++ server/src/app/clients/blackboard.js
@@ -176,7 +176,7 @@ class BlackBoardApiClient {
       console.log('getNewToken => request', request)
 
 			console.log('getNewToken => token', token)
-      cache.saveToken(newToken)
+      cache.saveToken(token)
 			return token
     } catch (e) {
 			console.log('Error getNewToken', e)
