09a86669b13b24c3e1b6b4c80de286eecfaaae79
Juan Luis Grandon <jota.lg19@gmail.com>
2025-10-20 17:12:37 -0300

    fix (bff/units): update misspelled function call

--- server/src/app/handlers/content.js
+++ server/src/app/handlers/content.js
@@ -7,7 +7,7 @@ export async function getContentsByCourseId (courseId, ids) {
     let contents = []
     contents = await cache.getContents(courseId)
     const cachedIds = contents.map(c => id)
-    const notCached = ids.filter(id => !cachedIds.include(id))
+    const notCached = ids.filter(id => !cachedIds.includes(id))
 
     //console.log('getContentsById => cachedColumnId' , cachedColumnId)
 
