7094444a25ae2250920c0da409d5409b500ca1c8
Juan Luis Grandon <jota.lg19@gmail.com>
2025-10-20 18:10:11 -0300

    fix: add logs

--- server/src/app/handlers/grades.js
+++ server/src/app/handlers/grades.js
@@ -10,13 +10,19 @@ export async function getGrade (
     let searchedGrade = null
 
     const cachedGrades = await cache.getGrades(courseId, columnId)
+    console.log('getGrade => cachedGrades', cachedGrades)
     searchedGrade = cachedGrades.find(cg => cg.userId == studentId)
+    console.log('getGrade => searchedGrade', searchedGrade)
+
     if (!searchedGrade) {
+
         const apiClient = BlackBoardApiClient.getClient()
         const request = await apiClient.get(
             `/v2/courses/${courseId}/gradebook/columns/${columnId}/users`
         )
         grades = request.data.results
+        console.log('getGrade => grades', grades)
+
         cache.saveGrades(courseId, columnId, grades)
         searchedGrade = grades.find(g => g.userId == studentId)
     }
