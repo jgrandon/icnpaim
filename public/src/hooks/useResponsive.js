366050420a135e61ea8eab8cff11f262c852e779
Juan Luis Grandon <jota.lg19@gmail.com>
2025-10-23 17:58:51 -0300

    fix:  add console log to use responsive

--- public/src/hooks/useResponsive.js
+++ public/src/hooks/useResponsive.js
@@ -4,15 +4,16 @@ export function useResponsive () {
     const windowWidth = useRef(window.innerWidth);
     
     useEffect(() => {
-    const handleResize = () => {
-        windowWidth.current = window.innerWidth;
-    };
+        const handleResize = () => {
+            console.log('handle resize', window.innerWidth)
+            windowWidth.current = window.innerWidth;
+        };
 
-    window.addEventListener('resize', handleResize);
+        window.addEventListener('resize', handleResize);
 
-    return () => {
-        window.removeEventListener('resize', handleResize);
-    };
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
     }, []);
 
     return windowWidth
