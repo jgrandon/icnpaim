9b8d548ff523127e3a659120feac332d14973ea7
Juan Luis Grandon <jota.lg19@gmail.com>
2025-11-06 20:47:28 -0300

    fix (front/cards): udpate modal to avoid failing on first load

--- public/src/components/pages/dashboardView.js
+++ public/src/components/pages/dashboardView.js
@@ -839,8 +839,8 @@ class DashboardView extends React.Component {
           aria-labelledby="simple-modal-title"
           aria-describedby="simple-modal-description"
         >
-          {this.state.modalData.card.title}
-          {`Para acceder a este Control dirigete a Blackboard > Cursos > ${this.state.selectedCourse.title} > ${this.state.modalData.unit.title} > ${this.state.modalData.card.title}`}
+          {this.state.modalData.card?.title}
+          {`Para acceder a este Control dirigete a Blackboard > Cursos > ${this.state.selectedCourse?.title} > ${this.state.modalData.unit?.title} > ${this.state.modalData.card?.title}`}
           <Link
             to={{ pathname: this.state.modalData.card?.url }}
             target="_blank"
