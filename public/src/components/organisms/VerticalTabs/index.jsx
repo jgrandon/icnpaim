20d5fbea2a10962a596c79aa768dd84efffa909e
Juan Grandon <jgrandon@udla-servicios.udla.cl>
2026-06-12 12:06:16 -0400

    fix: remove conflicting typography

--- public/src/components/organisms/VerticalTabs/index.jsx
+++ public/src/components/organisms/VerticalTabs/index.jsx
@@ -3,9 +3,6 @@ import PropTypes from 'prop-types'
 import { makeStyles } from '@material-ui/core/styles'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
-import Typography from '@material-ui/core/Typography'
-import Box from '@material-ui/core/Box'
-
 function TabPanel(props) {
     const { children, value, index, ...other } = props
 
@@ -17,7 +14,7 @@ function TabPanel(props) {
             aria-labelledby={`vertical-tab-${index}`}
             {...other}
         >
-            {value === index && (<Typography>{children}</Typography>)}
+            {value === index && (children)}
         </div>
     )
 }
