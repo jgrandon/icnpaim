5fdd31a9b8c1cd743bf32f13f3a719540ca39930
Juan Grandon <jgrandon@udla-servicios.udla.cl>
2026-06-23 16:16:32 -0400

    fix: add postgres ssl config when running on server

--- server/src/app/db/postgres.js
+++ server/src/app/db/postgres.js
@@ -6,6 +6,9 @@ const pool = new Pool({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
+    ssl: process.env.NODE_ENV == 'development' 
+        ? false 
+        : { rejectUnauthorized: false }
 })
 
 pool.on('error', (err, client) => {
