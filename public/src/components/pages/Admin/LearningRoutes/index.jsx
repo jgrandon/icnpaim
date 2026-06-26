6b270c9a0422c27cdcf6fb7d718d94386deafecb
Juan Grandon <jgrandon@udla-servicios.udla.cl>
2026-06-26 14:06:01 -0400

    fix (learnignRoutesAdmin): - add a debounce timer after a change on contents was made - call api 5 seconds after contents where updated - call update contents api after adding-removing contents

--- public/src/components/pages/Admin/LearningRoutes/index.jsx
+++ public/src/components/pages/Admin/LearningRoutes/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import MultiNodeSlider from './MultiPickSlider'
 import SettingsIcon from '@material-ui/icons/Settings'
 import TooltipIconButton from '../../../organisms/TooltipIconButton'
@@ -9,14 +9,23 @@ import VerticalTabs from '../../../organisms/VerticalTabs'
 import ContentSelector from './ContentSelector'
 import { v4 as uuidv4 } from 'uuid'
 import ContentSorter from './ContentSorter'
+import API from '../../../../services/learningRoutes'
+import SavingState from './SavingState'
 
 export default function LearningRoutesAdmin ({ unit }) {
     const [ isModalOpen, setModalOpen ] = useState(false)
     const [ learningRoutes, setLearningRoutes ] = useState([])
     const [ selectedLR, setSelectedLR ] = useState(null) // Learning Route shown in modal
-
+    const [ savingState, setSavingState ] = useState('clean')
     const closeModal = () => setModalOpen(false)
     const openModal = () => setModalOpen(true)
+    const savingTimerRef = useRef(null)
+
+    useEffect(() => {
+        return () => {
+            if (savingTimerRef.current) clearTimeout(savingTimerRef.current)
+        }
+    }, [])
 
     const handleSettingsAccept = (data) => {
         console.log('handleSettingsAccept => data', data)
@@ -42,7 +51,7 @@ export default function LearningRoutesAdmin ({ unit }) {
             const otherLR = prev.filter(lr => lr.level != updatedLevel)
             // remove all non selected contents and sort
             const oldContents = updatedLR.contents.filter(
-                c => !!( selectedContents.find(sc => sc.id == sc.id) )
+                c => !!( selectedContents.find(sc => sc.id == c.id) )
             ).sort((a, b) => (a.order - b.order))
 
             // remove from selected all prev contents
@@ -59,14 +68,49 @@ export default function LearningRoutesAdmin ({ unit }) {
             // update Learning Routes
             const allLR = [
                 ...otherLR,
-                { ...updatedLR, contents: updatedContents}
+                { ...updatedLR, contents: updatedContents }
             ].sort((a, b) => (a.level - b.level))
             console.log('handleContentsAccept => setLearningRoutes', allLR)
+
+            setSelectedLR({ ...updatedLR, contents: updatedContents })
             return allLR
         })
 
+        saveChanges({
+            unitId: unit.id,
+            level: selectedLR.level,
+            contents: selectedLR.contents
+        })
         closeModal()
         // setSelectedLR(null)
+    }
+
+
+    const handleContentUpdate = async (contents) => {
+        const { level } = selectedLR
+        console.log('onchange', {level, contents})
+        setSavingState('touched')
+        if (savingTimerRef.current) clearTimeout(savingTimerRef.current)
+        savingTimerRef.current = setTimeout(async () => {
+            alert('5 seconds have passed!')
+            await saveChanges({
+                unitId: unit.id,
+                level,
+                contents
+            })
+        }, 5000)
+    }
+
+    const saveChanges = async (data) => {
+        alert('saveChanges')
+        try {
+            setSavingState('saving')
+            await API.updateContents(data)
+            setSavingState('saved')
+        } catch (error) {
+            console.warn('updateContents API ERROR', error)
+            setSavingState('error')
+        }
     }
 
     return (
@@ -111,22 +155,12 @@ export default function LearningRoutesAdmin ({ unit }) {
                                 </TooltipIconButton>
                             </div>
 
-                            <ContentSorter contents={lr.contents} />
-                        {/*}
-                            <ReactSortable list={state} setList={setState}>
-                                { lr.contents.map(c => (
-                                    {state.map((item) => (
-                                    <div key={item.id}>
-                                        <Content
-                                            key={uuidv4()}
-                                            content={c}
-                                            onClick={() => {}}
-                                        />
-                                    </div>
-                                    ))}
-                                ))}
-                            </ReactSortable>
-                            */}
+                            <ContentSorter
+                                contents={lr.contents}
+                                onChange={handleContentUpdate}
+                            />
+
+                            <SavingState state={savingState}/>
                         </div>
                     ))
                 }
