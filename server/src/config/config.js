efac3f4e4ac9c62d993725b95318637769f0a591
Juan Grandon <jgrandon@udla-servicios.udla.cl>
2026-07-09 17:21:44 -0400

    Merge fix/LTI-redirect-uri to main:
    Squashed commit of the following:
    
    commit 2be2f6c4dc98fe2d9001a115578eeb46d1362994
    Author: Juan Grandon <jgrandon@udla-servicios.udla.cl>
    Date:   Thu Jul 9 17:21:00 2026 -0400
    
        fix: read app url env var

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,36 +1,36 @@
-import _ from 'lodash';
-import configJson from '../../config/config.json';
-import * as fs from 'fs';
+import _ from 'lodash'
+import configJson from '../../config/config.json'
+import * as fs from 'fs'
 
 /**
  * Load the customized config values from the config.json data.
  *
  */
 
-let overrides = '';
+let overrides = ''
 
-let configResult = {};
+let configResult = {}
 if (fs.existsSync('server/config/config_override.json')) {
-  overrides = require('../../config/config_override.json');
-  configResult = _.defaultsDeep(overrides, configJson);
+    overrides = require('../../config/config_override.json')
+    configResult = _.defaultsDeep(overrides, configJson)
 }
 
-if (process.env.LTI_TEST_PROVIDER_DOMAIN) {
-  configResult['frontend_url'] = process.env.LTI_TEST_PROVIDER_DOMAIN;
+if (process.env.APP_URL) {
+    configResult['frontend_url'] = process.env.APP_URL //process.env.LTI_TEST_PROVIDER_DOMAIN;
 }
 if (process.env.LTI_TEST_PROVIDER_PORT) {
-  configResult['provider_port'] = process.env.LTI_TEST_PROVIDER_PORT;
+    configResult['provider_port'] = process.env.LTI_TEST_PROVIDER_PORT
 }
 if (process.env.PORT) {
-  configResult['provider_port'] = process.env.PORT;
+    configResult['provider_port'] = process.env.PORT
 }
 if (process.env.DATABASE_DIRECTORY) {
-  configResult['database_directory'] = process.env.DATABASE_DIRECTORY;
+    configResult['database_directory'] = process.env.DATABASE_DIRECTORY
 }
 
 // Asegurar que el directorio de base de datos termine con /
 if (configResult['database_directory'] && !configResult['database_directory'].endsWith('/')) {
-  configResult['database_directory'] += '/';
+    configResult['database_directory'] += '/'
 }
 
-export default _.defaultsDeep(configResult, configJson);
+export default _.defaultsDeep(configResult, configJson)
