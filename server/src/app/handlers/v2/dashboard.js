7c2dc29054fec87aee82fdc018ed7996c4c48870
Juan Grandon <jgrandon@udla-servicios.udla.cl>
2026-07-22 12:43:07 -0400

    fix: get bb_content_id on /dashboard api

--- server/src/app/handlers/v2/dashboard.js
+++ server/src/app/handlers/v2/dashboard.js
@@ -1,9 +1,6 @@
-import * as UnitsHandler from './units'
 import client from '../../db/postgres'
 
 export async function getUnitsWithCards(courseId = '1', studentId) {
-    // const response = await UnitsHandler.getAllUnits()
-    // console.log('response.data => ',typeof response.data)
     const res = await client.query(
         `SELECT
             u.*,
@@ -12,6 +9,7 @@ export async function getUnitsWithCards(courseId = '1', studentId) {
             c.title,
             c.type,
             c.url,
+            c.bb_content_id as bbcontentid,
             p.completed
         FROM unit AS u
         FULL OUTER JOIN content AS c
@@ -36,11 +34,11 @@ export async function getUnitsWithCards(courseId = '1', studentId) {
         const row = res.rows[i]
         console.log('getUnitsWithCards => for ', {i, row})
         const { id, name, color, position, bbId,
-            contentid, title, type, url, completed,
+            contentid, bbcontentid, title, type, url, completed,
             description, free_progress: freeProgress } = row
 
         const content = { id: contentid,
-            title, type, url, completed }
+            title, type, url, completed, contentId: bbcontentid }
         const unit = { id, name, description, color, position, freeProgress, bbId }
 
         const inArray = units.find(u => row.id==u.id )
@@ -58,95 +56,4 @@ export async function getUnitsWithCards(courseId = '1', studentId) {
             : u ) // bypass
     }
     return units
-}
-
-
-
-/* Translates attributes from WordPress to ICNPAIM context */
-function getUnitData (unit) {
-    const { id, status, title } = unit
-    const cards = safeJsonParse(unit.meta.unit_cards) ?? []
-    return {
-        id,
-        status,
-        title,
-        content: clearContent(unit.content.rendered),
-        color: unit.meta.color,
-        freeProgress: unit.meta.free_progress,
-        courseId: unit.meta.course_id,
-        cards,
-        //learningRoutes: getLearningRoutes(allCards),
-        contentId: unit.meta?.content_id ?? 0,
-    }
-}
-
-/*
-export async function getUnit (unitId, courseId) {
-    console.log('getUnit => unitId', unitId)
-    console.log('getUnit => courseId', courseId)
-
-    const response = await WordPressApi.client.get(
-        `/unit/${unitId}`
-    )
-    console.log('getUnit => response.data', response.data)
-    const retrievedUnit = getUnitData(response.data)
-    console.log('getUnit => retrievedUnit', retrievedUnit)
-    return retrievedUnit.courseId == courseId
-        ? retrievedUnit
-        : null
-}*/
-
-function clearContent(content) {
-    return content
-        .replaceAll('<p>','')
-        .replaceAll('</p>', '')
-}
-
-function fixScormUrl(cards, studentId) {
-    const userKey = 'UserId%7C'
-    return cards.map(c => {
-        const cardOldUserId = c.url.split(userKey)[1]?.split('&')[0]
-        if (!cardOldUserId) return c
-        const newUrl = c.url.replace(cardOldUserId, studentId )
-        return {
-            ...c,
-            url: newUrl
-        }
-    })
-}
-
-export function getLearningRoutes(cards) {
-    let routes = []
-    const iCards = cards.length
-    for (let i = 0; i < iCards; i++) {
-        console.log('getLearningRoutes => inside for', i)
-        const currentCard = cards[i] 
-        const routeId = getRouteId(currentCard)
-        const oldRoutes = routes[routeId] ?? []
-        const newRoutes = [...oldRoutes, currentCard]
-        routes[routeId] = sortCardsByWeight(newRoutes)
-    }
-    return routes
-}
-
-function getRouteId (card) {
-    try {
-        const id = parseInt(card.learningRoute) - 1
-        return id > 0 ? id : 0
-    } catch (e) {
-        console.warn('Error parsing learningRoute', card, error)
-        return 0
-    } 
-}
-
-function sortCardsByWeight (cards) {
-    const sortedCards = cards.sort((a,b) => a.peso - b.peso)
-    return sortedCards.map((c, index) => ({...c, index}))
-}
-
-export async function getCourseUnits (searchedCourse, studentId) {
-    const units = await getUnits(studentId)
-    return units.filter(
-        u => u.courseId == searchedCourse
-    )
 }
