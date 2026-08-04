521fe4d3a41867b3260708b719526e0d9047c50d
Juan Grandon <jgrandon@udla-servicios.udla.cl>
2026-08-04 17:05:24 -0400

    feature: get student progress from db

--- server/src/app/handlers/v2/student.js
+++ server/src/app/handlers/v2/student.js
@@ -1,4 +1,5 @@
 import client from '../../db/postgres'
+import { objectToCamelCase } from '../../lib/objectToCamelCase'
 
 export async function getStudentByBBid(bbId) {
     const res = await client.query(
@@ -30,4 +31,50 @@ export async function createStudent ({
     )
     const student = res.rows[0]
     return { ...student, bbId }
+}
+
+export async function getStudentsResults (subjectId) {
+    const res = await client.query(
+        `SELECT
+            s.id,
+            s.name,
+            s.bb_id,
+            u.id AS unit_id,
+            COUNT(*) AS progress
+        FROM student AS s
+        LEFT JOIN (
+            SELECT * FROM progress WHERE completed = TRUE
+        ) AS p ON s.id = p.student_id
+        JOIN content AS c ON p.content_id = c.id
+        JOIN unit AS u ON c.unit_id = u.id
+        WHERE u.enabled = TRUE
+            AND u.published = TRUE
+            AND u.subject_id = $1
+        GROUP BY s.id, u.id`,
+        [ subjectId ]
+    )
+    const data = (res.rows || []).map(d => objectToCamelCase(d))
+    let students = []
+
+    //TODO: replace reduce with a for
+    for(let i=0; i < data.length; i++) {
+        const current = data[i]
+        const { id, name, unitId, progress, bbId } = current
+        const unit = {
+            id: unitId,
+            progress
+        }
+        let existing = students.find(s => s.id = id)
+        if (!existing) {
+            students.push({
+                id,
+                name,
+                bbId,
+                units: { [unitId] : unit } })
+        } else {
+            existing.units[unitId] = unit
+        }
+    }
+    return students
+    
 }
