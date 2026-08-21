7cb33b3a8ad60da06f6a64e2c30de870368ac0af
Juan Grandon <jgrandon@udla-servicios.udla.cl>
2026-08-21 15:18:26 -0400

    feature: get student grades from dda

---
+++ server/src/app/handlers/v2/dda/grades.js
@@ -0,0 +1,25 @@
+import client from '../../../db/dda'
+import { objectToCamelCase } from '../../../lib/objectToCamelCase'
+
+export async function getStudentGrades(studentId, courseId) {
+    const res = await client.query(
+        `select 
+          gbm.pk1 AS gradebook_id,
+          gbm.title AS content_title,
+          gbm.crsmain_pk1 AS course_id,
+          gbm.course_contents_pk1 AS content_id,
+          gbg.average_score,
+          a.score,
+          cu.users_pk1 as user_id
+          FROM gradebook_main AS gbm
+          JOIN gradebook_grade AS gbg ON gbm.pk1 = gbg.gradebook_main_pk1
+          JOIN attempt AS a ON gbg.highest_attempt_pk1 = a.pk1
+          JOIN course_users AS cu ON gbg.course_users_pk1 = cu.pk1
+          JOIN course_main as cm ON cu.crsmain_pk1 = cm.pk1
+          WHERE  gbm.crsmain_pk1 = 85368
+          LIMIT 100;`,
+        [ externalId ]
+    )
+    const grades = res.rows.map(r => objectToCamelCase(r))
+    return grades
+}
