e11dbc2d424da4cfc03a430eba649b89581fe423
Juan Grandon <jgrandon@udla-servicios.udla.cl>
2026-08-28 14:41:31 -0400

    Merge fix/content-url-parsing into main
    Squashed commit of the following:
    
    commit 7329047e222e7f21a64f9d623a57904abe960f3d
    Author: Juan Grandon <jgrandon@udla-servicios.udla.cl>
    Date:   Fri Aug 28 14:21:19 2026 -0400
    
        fix: content url parsing error
    
    commit 3c74c6f5b59ff79f6b374c1405e6a2208465d682
    Author: Juan Grandon <jgrandon@udla-servicios.udla.cl>
    Date:   Fri Aug 28 13:55:04 2026 -0400
    
        fix: add harcoded blackboard student id for testing
    
    commit eec38f18d7b00076653f2b3b3983085cacf53cb3
    Author: Juan Grandon <jgrandon@udla-servicios.udla.cl>
    Date:   Fri Aug 28 13:08:27 2026 -0400
    
        fix:
        - add try/catch block to avoid errors on api
        - add logs for debugging
        - escape null values when using .replace to parse content url

--- server/src/app/handlers/v2/contents.js
+++ server/src/app/handlers/v2/contents.js
@@ -55,34 +55,56 @@ function processUrl ({
 }) {
     let url = originalUrl
     let bbContentId = null
-    const courseId = getCourseIdFromURL(originalUrl)
-    if (type == 'control') {
-        const isAdminUrl = originalUrl.indexOf('assessment/test') != -1
-        const match = isAdminUrl
-            ? originalUrl.match(/assessment\/test\/(.*?)(?=\?gradeitemView)/)
-            : originalUrl.match(/assessment\/(.*?)(?=\/overview)/)
-        if (match) {
-            bbContentId = match[1]
-            url = originalUrl.slice( 0, originalUrl.indexOf(match[0]))
-                + `assessment/${bbContentId}/overview?courseId=${courseId}`
+    try {
+        const courseId = getCourseIdFromURL(originalUrl)
+        if (type == 'control') {
+            const isAdminUrl = originalUrl.indexOf('assessment/test') != -1
+            const match = isAdminUrl
+                ? originalUrl.match(/assessment\/test\/(.*?)(?=\?gradeitemView)/)
+                : originalUrl.match(/assessment\/(.*?)(?=\/overview)/)
+            if (match) {
+                bbContentId = match[1]
+                url = originalUrl.slice( 0, originalUrl.indexOf(match[0]))
+                    + `assessment/${bbContentId}/overview?courseId=${courseId}`
+            }
+            // https://udla-staging.blackboard.com/ultra/courses/_89726_1/assessment/_4639208_1/overview?courseId=_89726_1
+            // https://udla-staging.blackboard.com/ultra/courses/_89726_1/assessment/test/_4639208_1?gradeitemView=details
+        } else if (type == 'scorm') {
+            const isAdminUrl = originalUrl.indexOf('scorm/overview') == -1
+            const match = isAdminUrl
+                ? originalUrl.match(/scorm\/(.*?)(?=\/columns)/)
+                : originalUrl.match(/scorm\/overview\/(.*)/)
+            if (match) {
+                bbContentId = match[1]
+                url = originalUrl.slice( 0, originalUrl.indexOf('scorm'))
+                    + `scorm/overview/${bbContentId}`
+            }
+            //https://udla-staging.blackboard.com/ultra/courses/_89726_1/scorm/overview/_4639218_1
+            //https://udla-staging.blackboard.com/ultra/courses/_89726_1/scorm/_4639218_1/columns/_1077428_1/submissions?courseId=_89726_1&gradeitemView=students
         }
-        // https://udla-staging.blackboard.com/ultra/courses/_89726_1/assessment/_4639208_1/overview?courseId=_89726_1
-        // https://udla-staging.blackboard.com/ultra/courses/_89726_1/assessment/test/_4639208_1?gradeitemView=details
-    } else if (type == 'scorm') {
-        const isAdminUrl = originalUrl.indexOf('scorm/overview') == -1
-        const match = isAdminUrl
-            ? originalUrl.match(/scorm\/(.*?)(?=\/columns)/)
-            : originalUrl.match(/scorm\/overview\/(.*)/)
-        if (match) {
-            bbContentId = match[1]
-            url = originalUrl.slice( 0, originalUrl.indexOf('scorm'))
-                + `scorm/overview/${bbContentId}`
-        }
-        //https://udla-staging.blackboard.com/ultra/courses/_89726_1/scorm/overview/_4639218_1
-        //https://udla-staging.blackboard.com/ultra/courses/_89726_1/scorm/_4639218_1/columns/_1077428_1/submissions?courseId=_89726_1&gradeitemView=students
+    
+        console.log('processUrl originalUrl => ',originalUrl)
+        console.log('processUrl finalUrl => ',url)
+        console.log('processUrl bbContentId => ',bbContentId)
+        
+        const cleanContentId = cleanBBId(bbContentId)
+        console.log('processUrl cleanContentId => ',cleanContentId)
+
+        return { url, bbContentId: cleanContentId }
+    } catch (e) {
+        console.error('ERROR: processUrl() => ', e)
+        return { url, bbContentId }
     }
-    const cleanContentId = bbContentId.replace('_1', '').replace('_', '')
-    return { url, bbContentId: cleanContentId }
+}
+
+function cleanBBId(identifier) {
+    if (identifier.startsWith('_')) {
+        identifier = identifier.slice(1);
+    }
+    if (identifier.endsWith('_1')) {
+        identifier = identifier.slice(0, -2);
+    }
+    return identifier;
 }
 
 function getCourseIdFromURL (url) {
